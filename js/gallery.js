22921d67e8469f970e6579a554e08be49ca02509
LeahCurtis79 <80548573+LeahCurtis79@users.noreply.github.com>
2021-06-07 19:09:13 -0500

    New Site

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+(function() {
+    [...document.querySelectorAll("img")].forEach(item=>{
+        item.onmouseover = () => {
+                item.classList.add('over');
+        }
+
+        item.onmouseout = () => {
+            item.classList.remove('over')
+        }
+    });
+
+    document.querySelector("img").classList.add('first');
+})();    
